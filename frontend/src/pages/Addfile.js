7399217963faeef2df98eebe1642fbc1f0bcf7a9
harsh sahu <harshsahu@harshs-MacBook-Air.local>
2022-09-01 17:42:28 +0530

    update name

--- frontend/src/pages/Addfile.js
+++ frontend/src/pages/Addfile.js
@@ -48,7 +48,7 @@ const Addfile = () => {
               className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="File name"
             >
-              Title name dal do
+              Title name
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
