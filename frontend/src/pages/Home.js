2bfa94185b619eac879f687bf3f43b989660125b
tanish510 <tanishjain510@gmail.com>
2022-09-05 14:53:57 +0530

    resize_on_popUp

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,18 +15,17 @@ import Modal from 'react-bootstrap/Modal';
 
 
 function MyVerticallyCenteredModal(props) {
-  console.log(props)
   const [mwidth, setWidth] = useState();
   const [mheight, setHeight] = useState();
+  useEffect(()=>{
+  },[mwidth])
   const [name, setName] = useState();
-
   function size(img){
 
     setWidth(  document.getElementById(img).clientWidth)
     setHeight(  document.getElementById(img).clientHeight)
   }
-
-
+  
 const DownloadToPng = async (img,w,h) => {
   if(w===undefined){
     const x = document.getElementById(img).clientWidth
@@ -39,7 +38,6 @@ const DownloadToPng = async (img,w,h) => {
     h=y
   }
 
-  
   const preset = presets.offscreen()
 
   async function toPng(data) {
@@ -47,7 +45,7 @@ const DownloadToPng = async (img,w,h) => {
       width,
       height
     } = data
-    console.log(width)
+
     const canvas = new OffscreenCanvas(width, height)
     const ctx = canvas.getContext('2d')
     const v = await Canvg.from(ctx, img, preset)
@@ -57,18 +55,16 @@ const DownloadToPng = async (img,w,h) => {
     const pngUrl = URL.createObjectURL(blob)
     return pngUrl
   }
-
-
-
-  
   toPng({
     width: w,
     height: h
   }).then((pngUrl) => {  
+
     saveas(pngUrl)
   })
 
 }
+
 
   return (
     <Modal
@@ -94,23 +90,30 @@ const DownloadToPng = async (img,w,h) => {
       </Modal.Header>
       <Modal.Body>
         <Row>
-        <Col>
+        <Col 
+        style={{overflow:"auto"}}
+         >
 
       <Card.Img
           variant="top"
+    
           src={props.user.url}
           id={props.user.url}
-        />
+          style={{
+            width:(mwidth+"px"),
+              height:(mheight+"px")
+          }} 
+        />  
           </Col>
           <div className="vr" />
           <Col>
 
 
-      <Accordion flush onClick={()=>{
-              size(props.user.url)
-            }}>
+      <Accordion flush >
               <Accordion.Item eventKey="0" size="sm">
-                <Accordion.Header>Adjust Size</Accordion.Header>
+                <Accordion.Header onClick={()=>{
+              size(props.user.url)
+            }}>Adjust Size</Accordion.Header>
                 <Accordion.Body>
                   <InputGroup size="sm" className="mb-3">
                     <InputGroup.Text id="inputGroup-sizing-sm">
@@ -132,7 +135,8 @@ const DownloadToPng = async (img,w,h) => {
                     <Form.Control
                       aria-label="Small"
                       aria-describedby="inputGroup-sizing-sm"
-                      onChange={(e) => setHeight( e.target.value )}
+                     // onChange={(e) => setHeight( e.target.value )}
+                      onChange={(e) => {setHeight( e.target.value );}}
                       // placeholder={mheight}
                       value={mheight}
                       // name={document.getElementById(brand.url).clientHeight}
@@ -173,9 +177,6 @@ function Home() {
 
   const[ open , opendata] = useState([])
 
-
-
-  // console.log(width)
   const printIt=async()=>{
     setListOfBrands((await sendBrandAPI())?.data?.data)
 
@@ -191,7 +192,6 @@ function Home() {
       <Row md={4} className="g-4">
       {/* {Array.from({ length: listOfbrands.length }).map((_, idx) => ( */}
 {listOfbrands.map(brand=>{
-  // console.log(brand);
   return (
     <Col>
     
