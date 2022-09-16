47ccbb4fe4949a83be77c2dbab4b228bead139bf
harsh sahu <harshsahu@harshs-MacBook-Air.local>
2022-09-16 10:35:22 +0530

    url change

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "../interceptor/interceptor";
-// const URL = "https://things-of-brands.herokuapp.com"
-const URL = "http://localhost:8080";
+const URL = "http://35.244.29.198/api";
+
 
 const uploadSingleFileAPI = async (fileObject) => {
   const config = {
