354aebe928a2403805fd5129e913f07043c57013
harsh sahu <harshsahu@harshs-MacBook-Air.local>
2022-09-19 10:58:20 +0530

    update url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "../interceptor/interceptor";
 // const URL = "http://35.244.29.198/api";
-const URL = "https://thingsofbrand.herokuapp.com";
+// const URL = "https://thingsofbrand.herokuapp.com";
+const URL = "http://localhost:8080";
 
 
 // const uploadSingleFileAPI = async (fileObject) => {
