46cdc9db364370514efb97e4e8a75b9c3a5edae6
Chanchal <chanchagupta1332@gmail.com>
2022-11-10 15:52:26 +0530

    pwa implemented

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
-import { Container, Row, Form, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import { searchBrand, clearSearchBrand } from "../store/actions/search-brands";
 import { connect } from "react-redux";
@@ -12,26 +19,16 @@ function NavigationBar({ getSearchBrand, clearSearchBrand, searchBrandData }) {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const getcompany = async () => {
+    // // console.log(searchBrandData);
+    // var set = new Set(searchBrandData?.data?.domain[0]);
+    // // down.innerHTML = JSON.stringify([...set])
+    // console.log(set);
+  };
 
-
-
-const getcompany = async() =>{
-// // console.log(searchBrandData);
-// var set = new Set(searchBrandData?.data?.domain[0]);
-// // down.innerHTML = JSON.stringify([...set])
-// console.log(set);
-
-
-
-}
-
-
-useEffect(()=>{
-getcompany()
-},[searchBrandData])
-
-
-
+  useEffect(() => {
+    getcompany();
+  }, [searchBrandData]);
 
   const [searchItem, setItems] = useState();
 
@@ -43,7 +40,7 @@ getcompany()
       setItems(null);
       console.log(location.pathname === "/MyStuff");
       if (location.pathname === "/MyStuff") {
-        console.log("first")
+        console.log("first");
         await getSearchBrand({
           title: "",
           email: user.email,
@@ -51,7 +48,7 @@ getcompany()
           description: "",
         });
       } else {
-        await getSearchBrand({ title: "" ,active: "1"});
+        await getSearchBrand({ title: "", active: "1" });
       }
     } else {
       // getSearchBrand({title:text});
@@ -73,8 +70,8 @@ getcompany()
           // active: "1",
           // description: text,
           // _id: "",
-          name :text,
-          domain:text,
+          name: text,
+          domain: text,
         });
       }
     }
@@ -83,7 +80,7 @@ getcompany()
   var numbers = [1, 2, 3, 4, 5];
   const handleGoogleSignIn = async () => {
     try {
-      await googleSignIn()
+      await googleSignIn();
       // console.log(object);
     } catch (error) {
       console.log(error);
@@ -102,112 +99,137 @@ getcompany()
   return (
     <>
       <Navbar expand="lg bg-white" sticky="top">
-        <Container fluid >
-            <Navbar.Brand
-              onClick={() => {
-                navigate("/home");
-              }}
-              className="bo"
+        <Container fluid>
+          <Navbar.Brand
+            onClick={() => {
+              navigate("/home");
+            }}
+            className="bo"
+          >
+            Things of Brand
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav
+              className="m-auto my-2"
+              id="search-box"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
             >
-              Things of Brand     
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav
-                className="m-auto my-2"
-                id="search-box"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
-              >
-              {
-                location.pathname === "/search"?
+              {location.pathname === "/search" ? (
                 <Form className="justify-content-center">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                  show={true}
-                  onChange={(e) => {
-                    sendData(e.target.value);
-                  }}
-                  value={searchItem || ""}
-                  list="browsers"
-                  name="myBrowser"
-                />
-                <datalist id="browsers">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    show={true}
+                    onChange={(e) => {
+                      sendData(e.target.value);
+                    }}
+                    value={searchItem || ""}
+                    list="browsers"
+                    name="myBrowser"
+                  />
+                  {/* <Autocomplete
+                    renderItem={(item, isHighlighted) => (
+                      <div
+                        style={{
+                          background: isHighlighted ? "lightgray" : "white",
+                        }}
+                      >
+                        {searchBrandData.data.name}
+                      </div>
+                    )}
+                    value={value}
+                    onChange={(e) => (value = e.target.value)}
+                    onSelect={(val) => (value = val)}
+                  /> */}
+                  {/* <Autocomplete
                   {searchBrandData.data.map((brandData) => {
                     return(<option value={brandData.name}/>);
                   })}
-                </datalist>
-              </Form>
-              :""
-              }
-              </Nav>
-
-              {user?.displayName ? (
-                <NavDropdown
-                  title={user?.displayName}
-                  id="collasible-nav-dropdown"
-                  align="end"                  
-                >                  
-                  <NavDropdown.Item
-                    onClick={() => {
-                      navigate("/addfile");
-                    }}
-                  >
-                    Upload File
-                  </NavDropdown.Item>
-                  <NavDropdown.Item
-                    onClick={() => {
-                      navigate("/MyStuff");
-                    }}
-                  >
-                    My Stuff
-                  </NavDropdown.Item>
-                  
-                  <NavDropdown.Item
-                    onClick={() => {
-                      navigate("/company");
-                    }}
-                  >
-                    Brands
-                  </NavDropdown.Item>
-                  
-                  <NavDropdown.Item
-                    onClick={() => {
-                      navigate("/account");
-                    }}
-                  >
-                    User Profile
-                  </NavDropdown.Item>
-
-                  <NavDropdown.Divider />
-
-                  <NavDropdown.Item
-                    onClick={() => {
-                      navigate("/search");
-                    }}
-                  >
-                    Explore
-                  </NavDropdown.Item>
-                  <NavDropdown.Item
-                   href="https://thingsofbrand.canny.io/feature-requests"
-                   target="_blank"
-                  >
-                   Feature Requests
-                  </NavDropdown.Item>
-
-                  <NavDropdown.Divider />
-
-                  <NavDropdown.Item onClick={handleSignOut}>
-                    logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (       
-                  <button type="button" className="btn btn-outline-primary" onClick={handleGoogleSignIn}>Get started</button>
+                /> */}
+                  {/* <datalist id="browsers">
+                  {searchBrandData.data.map((brandData) => {
+                    return(<option value={brandData.name}/>);
+                  })}
+                </datalist> */}
+                </Form>
+              ) : (
+                ""
               )}
-            </Navbar.Collapse>
+            </Nav>
+
+            {user?.displayName ? (
+              <NavDropdown
+                title={user?.displayName}
+                id="collasible-nav-dropdown"
+                align="end"
+              >
+                <NavDropdown.Item
+                  onClick={() => {
+                    navigate("/addfile");
+                  }}
+                >
+                  Upload File
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  onClick={() => {
+                    navigate("/MyStuff");
+                  }}
+                >
+                  My Stuff
+                </NavDropdown.Item>
+
+                <NavDropdown.Item
+                  onClick={() => {
+                    navigate("/company");
+                  }}
+                >
+                  Brands
+                </NavDropdown.Item>
+
+                <NavDropdown.Item
+                  onClick={() => {
+                    navigate("/account");
+                  }}
+                >
+                  User Profile
+                </NavDropdown.Item>
+
+                <NavDropdown.Divider />
+
+                <NavDropdown.Item
+                  onClick={() => {
+                    navigate("/search");
+                  }}
+                >
+                  Explore
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  href="https://thingsofbrand.canny.io/feature-requests"
+                  target="_blank"
+                >
+                  Feature Requests
+                </NavDropdown.Item>
+
+                <NavDropdown.Divider />
+
+                <NavDropdown.Item onClick={handleSignOut}>
+                  logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <button
+                type="button"
+                className="btn btn-outline-primary"
+                onClick={handleGoogleSignIn}
+              >
+                Get started
+              </button>
+            )}
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
--- frontend/src/pages/Pagination.js
+++ frontend/src/pages/Pagination.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import { BsFillArrowLeftCircleFill,BsFillArrowRightCircleFill } from "react-icons/bs";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
 
 function Pagination({ postsPerPage, totalPosts, paginate }) {
   const [index, setIndex] = useState(0);
@@ -11,40 +13,29 @@ function Pagination({ postsPerPage, totalPosts, paginate }) {
   useEffect(() => {}, [index]);
   const handlePrevious = () => {
     if (index > 0) {
-        document.getElementById("next").style.display = "block"
-        document.getElementById("previous").style.display = "block"
+      document.getElementById("next").style.display = "block";
+      document.getElementById("previous").style.display = "block";
       console.log("entered", index);
       paginate(pageNumbers[index - 1]);
       setIndex(index - 1);
-      if(index-1 === 0){
-        document.getElementById("previous").style.display = "none"
-  
+      if (index - 1 === 0) {
+        document.getElementById("previous").style.display = "none";
       }
-      
-        
-
     }
-    
-
-    
   };
   const handleNext = () => {
     if (index < pageNumbers.length - 1) {
       console.log("pagiate to = ", index + 1);
       paginate(pageNumbers[index + 1]);
-      document.getElementById("next").style.display = "block"
-      document.getElementById("previous").style.display = "block"
-    
+      document.getElementById("next").style.display = "block";
+      document.getElementById("previous").style.display = "block";
     }
     if (index !== pageNumbers.length - 1) {
       console.log("index seted = ", index + 1);
       setIndex(index + 1);
-      if(index + 1=== pageNumbers.length - 1)
-        document.getElementById("next").style.display = "none"
-      
+      if (index + 1 === pageNumbers.length - 1)
+        document.getElementById("next").style.display = "none";
     }
-     
-      
   };
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
@@ -53,15 +44,21 @@ function Pagination({ postsPerPage, totalPosts, paginate }) {
   return (
     <nav>
       <ul className="pagination">
-        {/* <button id="previous" onClick={() => handlePrevious()}>
-          {" "}
-          Previous
-        </button> */}
         <div className="m-auto d-flex">
-
-        <div id="previous" style={{display:"flex" ,marginRight:7,display:"none"}} onClick={() => handlePrevious()}><BsFillArrowLeftCircleFill style={{fontSize:27}}/>  </div>
-        <div id="next" style={{display:"flex"}} onClick={() => handleNext()}><BsFillArrowRightCircleFill style={{fontSize:27}}/></div>
-        
+          <div
+            id="previous"
+            style={{ display: "flex", marginRight: 7, display: "none" }}
+            onClick={() => handlePrevious()}
+          >
+            <BsFillArrowLeftCircleFill style={{ fontSize: 27 }} />{" "}
+          </div>
+          <div
+            id="next"
+            style={{ display: "flex" }}
+            onClick={() => handleNext()}
+          >
+            <BsFillArrowRightCircleFill style={{ fontSize: 27 }} />
+          </div>
         </div>
       </ul>
     </nav>
