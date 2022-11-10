53a20e032ebae45981fc3a99624094c204b3909f
Chanchal <chanchagupta1332@gmail.com>
2022-11-10 11:48:35 +0530

    previous and next logo

--- frontend/src/pages/Pagination.js
+++ frontend/src/pages/Pagination.js
@@ -1,57 +1,71 @@
-import React, { useEffect } from 'react';
-import {useState} from   'react';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import { BsFillArrowLeftCircleFill,BsFillArrowRightCircleFill } from "react-icons/bs";
 
-function Pagination({postsPerPage,totalPosts,paginate}) {
-    const [index,setIndex] = useState(0);
-    const [temp,setTemp] = useState(1);
-   
+function Pagination({ postsPerPage, totalPosts, paginate }) {
+  const [index, setIndex] = useState(0);
+  const [temp, setTemp] = useState(1);
 
-    const pageNumbers= [];
-    useEffect(() => {
-
-      },[index]);
-    const handlePrevious = () =>{
+  const pageNumbers = [];
+  useEffect(() => {}, [index]);
+  const handlePrevious = () => {
+    if (index > 0) {
+        document.getElementById("next").style.display = "block"
+        document.getElementById("previous").style.display = "block"
+      console.log("entered", index);
+      paginate(pageNumbers[index - 1]);
+      setIndex(index - 1);
+      if(index-1 === 0){
+        document.getElementById("previous").style.display = "none"
+  
+      }
+      
         
-        if(index > 0 )
-        {
-            console.log("entered",index);
-            paginate(pageNumbers[index-1]) 
-            setIndex(index-1);
-        }
-       
-    }
-    const handleNext = () =>{
 
-        if(index < pageNumbers.length-1)
-        {
-            console.log("pagiate to = ",index+1);
-            paginate(pageNumbers[index+1]);
+    }
+    
 
-        }
-        if(index !== pageNumbers.length-1)
-        {
-            console.log("index seted = ",index+1);
-            setIndex(index+1);
-        }
+    
+  };
+  const handleNext = () => {
+    if (index < pageNumbers.length - 1) {
+      console.log("pagiate to = ", index + 1);
+      paginate(pageNumbers[index + 1]);
+      document.getElementById("next").style.display = "block"
+      document.getElementById("previous").style.display = "block"
+    
     }
-   
-    for(let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++){
-        pageNumbers.push(i);
+    if (index !== pageNumbers.length - 1) {
+      console.log("index seted = ", index + 1);
+      setIndex(index + 1);
+      if(index + 1=== pageNumbers.length - 1)
+        document.getElementById("next").style.display = "none"
+      
     }
+     
+      
+  };
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
   return (
     <nav>
-        <ul className="pagination">
-        <button id = "previous"  onClick={
-            ()=>
-        handlePrevious()
-    }> Previous</button>
+      <ul className="pagination">
+        {/* <button id="previous" onClick={() => handlePrevious()}>
+          {" "}
+          Previous
+        </button> */}
+        <div className="m-auto d-flex">
 
-                    <button id = "next" onClick={()=>handleNext()}>Next</button>
-
-        </ul>
+        <div id="previous" style={{display:"flex" ,marginRight:7,display:"none"}} onClick={() => handlePrevious()}><BsFillArrowLeftCircleFill style={{fontSize:27}}/>  </div>
+        <div id="next" style={{display:"flex"}} onClick={() => handleNext()}><BsFillArrowRightCircleFill style={{fontSize:27}}/></div>
+        
+        </div>
+      </ul>
     </nav>
-
   );
 }
 
-export default Pagination
+export default Pagination;
