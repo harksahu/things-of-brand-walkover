afd161207f77fc9a7a7e5e46a2ba947190eb2225
harsh sahu <sahupavan000@gmail.com>
2022-12-20 19:06:19 +0530

    update collection api

--- backend/controllers/CollectionController.js
+++ backend/controllers/CollectionController.js
@@ -19,7 +19,6 @@ const createCollection = async (req, res) => {
     }
 }
 
-
 const getCollectionDetails = async (req, res) => {
     const CollectionName = req?.query?.CollectionName
     const email = req?.query?.email
@@ -51,53 +50,33 @@ const getCollectionDetails = async (req, res) => {
 
 const updateCollection = async (req, res) => {
 
-    //     let { name, aboutus, fontLink } = req.body
-    //     let logo = req.body.logo;
-    //     let links = req.body.links;
-    //     let domain = req.body.domain;
-    //     let guidlines = req.body.guidlines;
-    //     let color = req.body.color;
-    //     let email = req.body.email;
-    //     let verify = req.body.verify
-    //     let link = req.body.links;
-    //     let id = req.body._id;
-    //     let sharedEmail = req.body.sharedEmail ? req.body.sharedEmail : "";
-    //     console.log("shared email", id)
-    //     try {
-    //         const data = await CollectionModel.updateMany(
-    //             {
-    //                 _id: id
-    //             },
-    //             {
-    //                 $set: {
-    //                     name,
-    //                     aboutus,
-    //                     logo,
-    //                     domain,
-    //                     links,
-    //                     guidlines,
-    //                     color,
-    //                     email,
-    //                     verify,
-    //                     link,
-    //                     fontLink,
-    //                     sharedEmail,
-    //                 }
-    //             }
-    //         )
+        let { CollectionName, _id, Logos } = req.body
+        try {
+            const data = await CollectionModel.updateMany(
+                {
+                    _id: _id
+                },
+                {
+                    $set: {
+                        CollectionName,
+                        Logos
+                    }
+                }
+            )
 
-    //         res.json({
-    //             "message": "Related Data is Successfully updated",
-    //             "data": data
-    //         }).status(200);
-    //     } catch (error) {
-    //         // console.log(error);
-    //         res.send({
-    //             message: "Some Error on Server",
-    //             error
-    //         }).status(400);
-    //     }
+            res.json({
+                "message": "Related Data is Successfully updated",
+                "data": data
+            }).status(200);
+        } catch (error) {
+            // console.log(error);
+            res.send({
+                message: "Some Error on Server",
+                error
+            }).status(400);
+        }
 }
+
 
 const getCollectionJson = async (domain) => {
     //     try {
