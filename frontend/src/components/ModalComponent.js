45c3710cffaf42fe92e1011e1e1904cd07a612e9
harsh sahu <sahupavan000@gmail.com>
2022-12-27 12:40:21 +0530

    collection update

--- frontend/src/components/ModalComponent.js
+++ frontend/src/components/ModalComponent.js
@@ -16,27 +16,30 @@ function ModalComponent(props) {
   const [collectionName, setCollectionName] = useState("");
   const [showComponent, setShowComponent] = useState(false);
   const { user } = UserAuth();
-
   useEffect(() => {
-    console.log(props?.allcollection?.data?.data);
-    setShowComponent(false);
     setDuplicateError(false);
-    setCollection(props?.allcollection?.data?.data);
+  }, []);
+  useEffect(() => {
+
+    setShowComponent(false);
+    setCollection(props?.allcollection?.data?.data || collectionName);
     setId(props?.id);
     setLogos(props?.allcollection?.data?.data);
   }, [props]);
 
   const createCollections = async (collectionName) => {
-    console.log(collectionName);
+
     const ans = await createCollection({
       CollectionName: collectionName,
       email: user.email,
       Logos: [],
     });
     if (ans) {
+
       alert("collection created successfully");
     }
-    // console.log("ans",ans)
+
+
   };
 
   const createNewCollection = async (collection, logo_id) => {
@@ -48,8 +51,7 @@ function ModalComponent(props) {
       allLogos.push(logo_id);
       var temp = props.variants;
       temp[props.index] = "red";
-      console.log(props.index);
-      console.log(temp);
+
       props.setVariants([...temp]);
     }
     const data = await updateCollection({
@@ -62,6 +64,7 @@ function ModalComponent(props) {
       props.setAddedCollection(true);
     }
   };
+
   return (
     <Modal
       {...props}
@@ -69,7 +72,7 @@ function ModalComponent(props) {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      {console.log("show component " + showComponent)}
+
 
       <Modal.Header closeButton>
         {/* <Link to="/collection"> */}
@@ -108,13 +111,13 @@ function ModalComponent(props) {
       </Modal.Header>
       <Modal.Body>
         {!props?.allcollection?.data?.data?.length ? (
-          <Card 
+          <Card
             onClick={() => {
               setShowComponent(true);
             }}
-            className="h-100 item-company add-new"
+            className="h-100 item-company add-new d-flex"
           >
-            <Card.Body className="add-icon align-items-center d-flex justify-content-center">
+            <Card.Body className="add-icon align-items-center justify-content-center">
               <Card.Title className="text-center">
                 <BsFillPlusCircleFill style={{ fontSize: 40 }} />
               </Card.Title>
@@ -128,36 +131,36 @@ function ModalComponent(props) {
         ) : (
           ""
         )}
+        <div className="d-flex item m-3" style={{overflow : "scroll"}}>
+          {collection &&
+            collection.map((collection) => {
+              return (
+                <div key={collection._id} className="m-3">
+                  <div >
+                    {/* <Link to={"/collection/" +collection._id}> */}
 
-        {collection &&
-          collection.map((collection) => {
-            return (
-              <div key={collection._id}>
-                <div className="d-flex justify-content-center item ">
-                  {/* <Link to={"/collection/" +collection._id}> */}
-                 
-                  <Card style={{height: "7.5rem" ,  width:"8rem"}}
-                    className="item-company"
-                    onClick={() => {
-                      createNewCollection(collection, id);
-                    }}
-                  >
-                    <div
-                      
-                      className="img_size  pattern-square"
+                    <Card style={{ height: "7.5rem", width: "8rem" }}
+                      className="item-company"
+                      onClick={() => {
+                        createNewCollection(collection, id);
+                      }}
                     >
-                      {collection?.logo[0]?.url !== undefined &&
-                      collection?.logo[0]?.url !== "null" ? (
-                        <img style={{height: "4rem" ,  width:"5rem" }} src={collection?.logo[0]?.url} alt="" />
-                      ) : (
-                        <img style={{height: "5rem" ,  width:"5rem" }} src="/assets/picture.svg" alt="" />
-                      )}
-                    </div>
-                    <div style={{textalign:"center" , paddingLeft:"18%"}}>
+                      <div
 
-                    {collection.CollectionName}
-                    </div>
-                    {/* <Card.Body>
+                        className="img_size  pattern-square"
+                      >
+                        {collection?.logo[0]?.url !== undefined &&
+                          collection?.logo[0]?.url !== "null" ? (
+                          <img style={{ height: "4rem", width: "5rem" }} src={collection?.logo[0]?.url} alt="" />
+                        ) : (
+                          <img style={{ height: "5rem", width: "5rem" }} src="/assets/picture.svg" alt="" />
+                        )}
+                      </div>
+                      <div style={{ textalign: "center", paddingLeft: "18%" }}>
+
+                        {collection.CollectionName}
+                      </div>
+                      {/* <Card.Body>
                       <Card.Title 
                         style={{ textDecoration: "none" , paddingRight:"65%"  }}
                         className="text-center"
@@ -165,13 +168,14 @@ function ModalComponent(props) {
                         {collection.CollectionName}
                       </Card.Title>
                     </Card.Body> */}
-                  </Card>
+                    </Card>
                   </div>
                   {/* </Link> */}
                 </div>
-         
-            );
-          })}
+
+              );
+            })}
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
--- frontend/src/pages/MyCollection.js
+++ frontend/src/pages/MyCollection.js
@@ -8,6 +8,7 @@ import "../scss/company.scss";
 import "../utils/SvgInLine.css";
 import { createCollection, getCollection } from "../api/Index.js";
 import { Link} from "react-router-dom";
+import InputComponent from "../components/InputComponent";
 const MyCollection = () => {
   const [allCollection, setAllCollection] = useState([]);
   const [show, setShow] = useState(false);
@@ -51,8 +52,10 @@ const MyCollection = () => {
           <Modal.Title>Create Your collection</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form.Group className="mb-3">
-            {showSuccess&&<Form.Label>Collection Created</Form.Label>}
+        {/* label, setValue, valuee, placeholderr } */}
+        <InputComponent value ={collectionName} setValue ={setCollectionName}label ={"Add new collection"}  placeholder={"Enter collection name"}/>
+          {/* <Form.Group className="mb-3"> */}
+            {/* {showSuccess&&<Form.Label>Collection Created</Form.Label>}
             <Form.Control
               type="domain"
               placeholder="Enter collection name"
@@ -63,8 +66,8 @@ const MyCollection = () => {
               onChange={(e) => {
                 setCollectionName(e.target.value);
               }}
-            />
-            <br></br>
+            /> */}
+            {/* <br></br> */}
             <Button
               variant="primary"
               onClick={() => {
@@ -73,7 +76,7 @@ const MyCollection = () => {
             >
               Next
             </Button>
-          </Form.Group>
+          {/* </Form.Group> */}
         </Modal.Body>
       </Modal>
       <Container>
