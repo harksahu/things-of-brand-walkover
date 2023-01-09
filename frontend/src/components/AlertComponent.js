e0da64fed62a0125cc8ac87b48b11556c586b1e8
Ashish Yadav <ashish@walkover.in>
2023-01-09 22:01:36 +0530

    conflicts

--- frontend/src/components/AlertComponent.js
+++ frontend/src/components/AlertComponent.js
@@ -2,6 +2,7 @@
 import React, { useRef } from "react";
 
 import Toast from "react-bootstrap/Toast";
+import ToastContainer from 'react-bootstrap/ToastContainer';
 
 const AlertComponent = ({message,showAlert,setShowAlert}) => {
     const show1 = useRef(false);
@@ -12,18 +13,24 @@ const AlertComponent = ({message,showAlert,setShowAlert}) => {
     // },[show])
   return (
     <div>
+      <ToastContainer position="top-end" className="p-3">
         <Toast
-            ref={show1}
+          ref={show1}
           onClose={() => setShowAlert(false)}
+          bg={'success'}
           show={showAlert}
           delay={3000}
           autohide
         >
-          <Toast.Header>{message}</Toast.Header>
+          <Toast.Header>            
+            <strong className="me-auto">Alert</strong>
+            <small className="text-muted">Close</small>
+          </Toast.Header>
+          <Toast.Body>{message}</Toast.Body>
         </Toast>
-      
+      </ToastContainer>
     </div>
   )
 }
 
-export default AlertComponent
+export default AlertComponent;
